7913bbe8e60d7b37f2c9d6252f08482f27cec1cf
xHyroM <generalkubo@gmail.com>
2023-08-04 16:14:45 +0200

    feat(api): add some basic event interfaces

---
+++ api/src/main/java/dev/xhyrom/lighteco/api/event/currency/CurrencyRegisterEvent.java
@@ -0,0 +1,11 @@
+package dev.xhyrom.lighteco.api.event.currency;
+
+import dev.xhyrom.lighteco.api.event.LightEcoEvent;
+import dev.xhyrom.lighteco.api.event.util.Position;
+import dev.xhyrom.lighteco.api.model.currency.Currency;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public interface CurrencyRegisterEvent extends LightEcoEvent {
+    @Position(0)
+    @NonNull Currency<?> getCurrency();
+}
