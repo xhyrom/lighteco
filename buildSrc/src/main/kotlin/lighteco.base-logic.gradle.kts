1d808680d13e7bed171bbbfaaaf03f4b5f3fde28
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-09-28 17:01:16 +0200

    ci: add publishing to repo.jopga.me

--- buildSrc/src/main/kotlin/lighteco.base-logic.gradle.kts
+++ buildSrc/src/main/kotlin/lighteco.base-logic.gradle.kts
@@ -24,3 +24,27 @@ tasks {
         options.encoding = Charsets.UTF_8.name()
     }
 }
+
+publishing {
+    // Publishing to repo.jopga.me
+    publications.create<MavenPublication>("mavenJava") {
+        repositories.maven {
+            url = uri("https://repo.jopga.me/releases")
+
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        groupId = rootProject.group as String
+        artifactId = project.name
+        version = rootProject.version as String
+
+        pom {
+            name.set("LightEco")
+        }
+
+        artifact(tasks.named("jar"))
+    }
+}
